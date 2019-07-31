aebc156a07c4010f20b6455be3692ceedfd74da8
Yini <ydxb7@mail.missouri.edu>
2019-07-31 14:14:09 -0700

    7. SectionsPagerAdapter Tabs

--- app/src/main/java/ai/tomorrow/instagramclone/Home/HomeActivity.java
+++ app/src/main/java/ai/tomorrow/instagramclone/Home/HomeActivity.java
@@ -1,6 +1,8 @@
 package ai.tomorrow.instagramclone.Home;
 
 import android.content.Context;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -25,7 +27,29 @@ public class HomeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_home);
         Log.d(TAG, "onCreate: starting");
         setupBottomNavigationView();
+        setupViewPager();
     }
+
+
+    /**
+     * Responsible for adding the 3 tabs: Camera, Home, Messages
+     */
+    private void setupViewPager(){
+        SectionsPagerAdapter adapter = new SectionsPagerAdapter(getSupportFragmentManager());
+        adapter.addFragment(new CameraFragment()); // index 0
+        adapter.addFragment(new HomeFragment()); // index 1
+        adapter.addFragment(new MessagesFragment()); // index 2
+        ViewPager viewPager = (ViewPager) findViewById(R.id.container);
+        viewPager.setAdapter(adapter);
+
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(viewPager);
+
+        tabLayout.getTabAt(0).setIcon(R.drawable.ic_camera);
+        tabLayout.getTabAt(1).setIcon(R.drawable.ic_action_name);
+        tabLayout.getTabAt(2).setIcon(R.drawable.ic_arrow);
+    }
+
 
     /**
      * BottomNavigationView setup
