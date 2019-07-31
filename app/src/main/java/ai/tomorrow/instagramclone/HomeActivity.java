cb98cf24658e8b03d4eaab3ddb64775f4bb7a5a3
Yini <ydxb7@mail.missouri.edu>
2019-07-31 11:35:01 -0700

    4. Customizing the BottomNavigationView

--- app/src/main/java/ai/tomorrow/instagramclone/HomeActivity.java
+++ app/src/main/java/ai/tomorrow/instagramclone/HomeActivity.java
@@ -2,12 +2,30 @@ package ai.tomorrow.instagramclone;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
+
+import ai.tomorrow.instagramclone.Utils.BottomNavigationViewHelper;
 
 public class HomeActivity extends AppCompatActivity {
+
+    private static final String TAG = "HomeActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        Log.d(TAG, "onCreate: starting");
+        setupBottomNavigationView();
     }
+
+    /**
+     * BottomNavigationView setup
+     */
+    private void setupBottomNavigationView(){
+        BottomNavigationViewEx bottomNavigationViewEx = (BottomNavigationViewEx) findViewById(R.id.bottomNavViewBar);
+        BottomNavigationViewHelper.setupBottomNavigationView(bottomNavigationViewEx);
+    }
+
 }
---
+++ app/src/main/java/ai/tomorrow/instagramclone/Utils/BottomNavigationViewHelper.java
@@ -0,0 +1,18 @@
+package ai.tomorrow.instagramclone.Utils;
+
+import android.util.Log;
+
+import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
+
+public class BottomNavigationViewHelper {
+
+    private static final String TAG = "BottomNavigationViewHel";
+
+    public static void setupBottomNavigationView(BottomNavigationViewEx bottomNavigationViewEx){
+        Log.d(TAG, "setupBottomNavigationView: Setting up BottomNavigationView");
+        bottomNavigationViewEx.enableAnimation(false);
+        bottomNavigationViewEx.enableItemShiftingMode(false);
+        bottomNavigationViewEx.enableShiftingMode(false);
+        bottomNavigationViewEx.setTextVisibility(false);
+    }
+}
