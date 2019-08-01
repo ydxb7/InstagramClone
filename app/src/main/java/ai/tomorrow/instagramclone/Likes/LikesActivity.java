c6d4db2867c1a7e12573c86f04ef1fd6b65cbcd7
Yini <ydxb7@mail.missouri.edu>
2019-08-01 14:26:14 -0700

    19. Register Layout

--- app/src/main/java/ai/tomorrow/instagramclone/Likes/LikesActivity.java
+++ app/src/main/java/ai/tomorrow/instagramclone/Likes/LikesActivity.java
@@ -22,7 +22,7 @@ public class LikesActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_register);
         Log.d(TAG, "onCreate: starting");
 //        setupBottomNavigationView();
     }
