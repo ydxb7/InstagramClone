3e5ea0833a6aaafb379dbd9dae3f69fa5e655181
Yini <ydxb7@mail.missouri.edu>
2019-08-01 10:55:26 -0700

    15. Universal Image Loader Config

--- app/src/main/java/ai/tomorrow/instagramclone/Profile/EditProfileFragment.java
+++ app/src/main/java/ai/tomorrow/instagramclone/Profile/EditProfileFragment.java
@@ -4,20 +4,42 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.nostra13.universalimageloader.core.ImageLoader;
 
 import ai.tomorrow.instagramclone.R;
+import ai.tomorrow.instagramclone.Utils.UniversalImageLoader;
 
 public class EditProfileFragment extends Fragment {
     private static final String TAG = "EditProfileFragment";
+    private ImageView mProfilePhoto;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_editprofile, container, false);
+        mProfilePhoto = view.findViewById(R.id.profile_photo);
+
+        initImageLoader();
+
+        setProfileImage();
 
         return view;
+    }
+
+    private void initImageLoader(){
+        UniversalImageLoader universalImageLoader = new UniversalImageLoader(getActivity());
+        ImageLoader.getInstance().init(universalImageLoader.getConfig());
+    }
+
+    private void setProfileImage(){
+        Log.d(TAG, "setProfileImage: setting profile image.");
+        String imgURL = "http://k2.jsqq.net/uploads/allimg/1711/17_171129092304_1.jpg";
+        UniversalImageLoader.setImage(imgURL, mProfilePhoto, null, "");
     }
 }
---
+++ app/src/main/java/ai/tomorrow/instagramclone/Utils/UniversalImageLoader.java
@@ -0,0 +1,88 @@
+package ai.tomorrow.instagramclone.Utils;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+
+import com.nostra13.universalimageloader.cache.memory.impl.WeakMemoryCache;
+import com.nostra13.universalimageloader.core.DisplayImageOptions;
+import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
+import com.nostra13.universalimageloader.core.assist.FailReason;
+import com.nostra13.universalimageloader.core.assist.ImageScaleType;
+import com.nostra13.universalimageloader.core.display.FadeInBitmapDisplayer;
+import com.nostra13.universalimageloader.core.listener.ImageLoadingListener;
+
+import ai.tomorrow.instagramclone.R;
+
+public class UniversalImageLoader {
+
+    private static final int defaultImage = R.drawable.ic_android;
+    private Context mContext;
+
+    public UniversalImageLoader(Context mContext) {
+        this.mContext = mContext;
+    }
+
+    public ImageLoaderConfiguration getConfig(){
+        DisplayImageOptions defaultOptions = new DisplayImageOptions.Builder()
+                .showImageOnLoading(defaultImage)
+                .showImageForEmptyUri(defaultImage)
+                .showImageOnFail(defaultImage)
+                .cacheOnDisk(true).cacheInMemory(true)
+                .cacheOnDisk(true).resetViewBeforeLoading(true)
+                .imageScaleType(ImageScaleType.EXACTLY)
+                .displayer(new FadeInBitmapDisplayer(300)).build();
+
+        ImageLoaderConfiguration configuration = new ImageLoaderConfiguration.Builder(mContext)
+                .defaultDisplayImageOptions(defaultOptions)
+                .memoryCache(new WeakMemoryCache())
+                .diskCacheSize(100 * 1024 * 1024).build();
+
+        return configuration;
+    }
+
+    /**
+     * this method can be used set images that are static. It can't be used if the images are being
+     * changed in the Fragment/Activity - Or if they are being set in a list or a grid view.
+     * @param imgURL
+     * @param image
+     * @param mProgressBar
+     * @param append
+     */
+    public static void setImage(String imgURL, final ImageView image, final ProgressBar mProgressBar, String append){
+        ImageLoader imageLoader = ImageLoader.getInstance();
+        imageLoader.displayImage(append + imgURL, image, new ImageLoadingListener() {
+            @Override
+            public void onLoadingStarted(String imageUri, View view) {
+                if (mProgressBar != null){
+                    mProgressBar.setVisibility(View.VISIBLE);
+                }
+            }
+
+            @Override
+            public void onLoadingFailed(String imageUri, View view, FailReason failReason) {
+                if (mProgressBar != null){
+                    mProgressBar.setVisibility(View.GONE);
+                }
+            }
+
+            @Override
+            public void onLoadingComplete(String imageUri, View view, Bitmap loadedImage) {
+                if (mProgressBar != null){
+                    mProgressBar.setVisibility(View.GONE);
+                }
+            }
+
+            @Override
+            public void onLoadingCancelled(String imageUri, View view) {
+                if (mProgressBar != null){
+                    mProgressBar.setVisibility(View.GONE);
+                }
+            }
+        });
+
+    }
+}
