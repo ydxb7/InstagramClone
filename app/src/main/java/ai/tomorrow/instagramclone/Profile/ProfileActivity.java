11a9160f7ae18c046e839eefa25532996478173f
Yini <ydxb7@mail.missouri.edu>
2019-08-01 03:53:18 -0700

    9. Building the User Profile

--- app/src/main/java/ai/tomorrow/instagramclone/Profile/ProfileActivity.java
+++ app/src/main/java/ai/tomorrow/instagramclone/Profile/ProfileActivity.java
@@ -25,7 +25,7 @@ public class ProfileActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
         Log.d(TAG, "onCreate: starting");
-//        setupBottomNavigationView();
+        setupBottomNavigationView();
         setupToolbar();
     }
 
