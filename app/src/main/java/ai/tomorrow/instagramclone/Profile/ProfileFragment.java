406c45dc39e3fb818ee6f8341b6cf3625c15a090
Yini <ydxb7@mail.missouri.edu>
2019-08-03 09:44:53 -0700

    29. Profile Fragment

---
+++ app/src/main/java/ai/tomorrow/instagramclone/Profile/ProfileFragment.java
@@ -0,0 +1,27 @@
+package ai.tomorrow.instagramclone.Profile;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import ai.tomorrow.instagramclone.R;
+
+public class ProfileFragment extends Fragment {
+
+    private static final String TAG = "ProfileFragment";
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_profile, container, false);
+
+
+
+        return view;
+    }
+}
