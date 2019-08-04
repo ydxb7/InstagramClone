4857b4fff5b3bc7366d6dfc4d8d0ebf9f732988e
Yini <ydxb7@mail.missouri.edu>
2019-08-04 15:10:43 -0700

    45. GalleryFragment Layout

--- app/src/main/java/ai/tomorrow/instagramclone/Share/GalleryFragment.java
+++ app/src/main/java/ai/tomorrow/instagramclone/Share/GalleryFragment.java
@@ -5,6 +5,11 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -14,6 +19,14 @@ import ai.tomorrow.instagramclone.R;
 
 public class GalleryFragment extends Fragment {
     private static final String TAG = "GalleryFragment";
+    
+    //widgets
+    private GridView gridView;
+    private ImageView galleryImage;
+    private ProgressBar progressBar;
+    private Spinner directorySpinner;
+    
+    
 
     @Nullable
     @Override
@@ -21,6 +34,34 @@ public class GalleryFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_gallery, container, false);
         Log.d(TAG, "onCreateView: stated.");
 
+        galleryImage = (ImageView) view.findViewById(R.id.galleryImageView);
+        gridView = (GridView) view.findViewById(R.id.gridView);
+        directorySpinner = (Spinner) view.findViewById(R.id.spinnerDirectory);
+        progressBar = (ProgressBar) view.findViewById(R.id.progressBar);
+        progressBar.setVisibility(View.GONE);
+        
+        ImageView shareClose = (ImageView) view.findViewById(R.id.ivCloseShare);
+        shareClose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "onClick: closing the gallery fragment");
+                getActivity().finish();
+            }
+        });
+
+        TextView nextScreen = (TextView) view.findViewById(R.id.tvNext);
+        nextScreen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "onClick: navigating to the final share screen");
+
+
+
+
+
+            }
+        });
+        
         return view;
     }
 }
