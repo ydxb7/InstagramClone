7ccef490a089ddde68b2b52a6f2fae11ec1af23f
Yini <ydxb7@mail.missouri.edu>
2019-08-04 16:15:05 -0700

    46. Phone Directories

--- app/src/main/java/ai/tomorrow/instagramclone/Share/GalleryFragment.java
+++ app/src/main/java/ai/tomorrow/instagramclone/Share/GalleryFragment.java
@@ -5,6 +5,8 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.GridView;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
@@ -15,7 +17,11 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import java.util.ArrayList;
+
 import ai.tomorrow.instagramclone.R;
+import ai.tomorrow.instagramclone.Utils.FilePaths;
+import ai.tomorrow.instagramclone.Utils.FileSearch;
 
 public class GalleryFragment extends Fragment {
     private static final String TAG = "GalleryFragment";
@@ -25,7 +31,9 @@ public class GalleryFragment extends Fragment {
     private ImageView galleryImage;
     private ProgressBar progressBar;
     private Spinner directorySpinner;
-    
+
+    //vars
+    private ArrayList<String> directories;
     
 
     @Nullable
@@ -61,7 +69,43 @@ public class GalleryFragment extends Fragment {
 
             }
         });
-        
+
+        init();
+
+
         return view;
     }
+
+    private void init(){
+        FilePaths filePaths = new FilePaths();
+
+        //check for other folders inside "/storage/emulated/0/pictures"
+        if (FileSearch.getDirectoryPaths(filePaths.PICTURES) != null){
+            directories = FileSearch.getDirectoryPaths(filePaths.PICTURES);
+        }
+        directories.add(filePaths.CAMERA);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(),
+                android.R.layout.simple_spinner_item, directories);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        directorySpinner.setAdapter(adapter);
+
+        directorySpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Log.d(TAG, "onItemSelected: selected: " + directories.get(position));
+
+                // setup our image grid for the directory chosen
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+    }
+
+
+
 }
