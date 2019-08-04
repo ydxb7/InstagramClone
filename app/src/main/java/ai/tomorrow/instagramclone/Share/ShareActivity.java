039967db50e1d523be531c4fcafff1ab2e3a307a
Yini <ydxb7@mail.missouri.edu>
2019-08-04 11:24:16 -0700

    41. Verifying Permissions for Sharing

--- app/src/main/java/ai/tomorrow/instagramclone/Share/ShareActivity.java
+++ app/src/main/java/ai/tomorrow/instagramclone/Share/ShareActivity.java
@@ -1,22 +1,28 @@
 package ai.tomorrow.instagramclone.Share;
 
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
 import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
 
 import ai.tomorrow.instagramclone.R;
 import ai.tomorrow.instagramclone.Utils.BottomNavigationViewHelper;
+import ai.tomorrow.instagramclone.Utils.Permissions;
 
 public class ShareActivity extends AppCompatActivity {
 
     private static final String TAG = "ShareActivity";
+
+    // constants
     private static final int ACTIVITY_NUM = 2;
+    private static final int VERIFY_PERMISSION_REQUEST = 1;
 
     private Context mContext = ShareActivity.this;
 
@@ -25,8 +31,66 @@ public class ShareActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
         Log.d(TAG, "onCreate: starting");
+
+        if (checkPermissionArray(Permissions.PERMISSIONS)){
+            // all permissions are granted
+        } else {
+            // verify permissions
+            verifyPermissions(Permissions.PERMISSIONS);
+        }
+
         setupBottomNavigationView();
     }
+
+    public void verifyPermissions(String[] permissions){
+        Log.d(TAG, "verifyPermissions: verifying permissions");
+
+        ActivityCompat.requestPermissions(
+                ShareActivity.this,
+                permissions,
+                VERIFY_PERMISSION_REQUEST
+        );
+    }
+
+    /**
+     * check an array of permissions are they have been granted
+     * @param permissions
+     * @return
+     */
+    public boolean checkPermissionArray(String[] permissions){
+        Log.d(TAG, "checkPermissionArray: checking permissions array.");
+
+        for (int i = 0; i < permissions.length; i++){
+            String check = permissions[i];
+            if (!checkPermissions(check)){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /**
+     * check a single permission is it has been granted
+     * @param permission
+     * @return
+     */
+    public boolean checkPermissions(String permission){
+        Log.d(TAG, "checkPermissions: checking permission: " + permission);
+
+        int permissionRequest = ActivityCompat.checkSelfPermission(ShareActivity.this, permission);
+
+        if (permissionRequest != PackageManager.PERMISSION_GRANTED){
+            Log.d(TAG, "checkPermissions: Permission was not granted for: " + permission);
+            return false;
+        } else {
+            Log.d(TAG, "checkPermissions: Permission was granted for: " + permission);
+            return true;
+        }
+    }
+
+
+
+
 
     /**
      * BottomNavigationView setup
---
+++ app/src/main/java/ai/tomorrow/instagramclone/Utils/Permissions.java
@@ -0,0 +1,12 @@
+package ai.tomorrow.instagramclone.Utils;
+
+import android.Manifest;
+
+public class Permissions {
+
+    public static final String[] PERMISSIONS = {
+            Manifest.permission.WRITE_EXTERNAL_STORAGE,
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.CAMERA
+    };
+}
