4fe47e4b42dbeb03d3e781709fb28e240339bb18
Yini <ydxb7@mail.missouri.edu>
2019-08-04 17:35:19 -0700

    47. ShareActivity GridView

--- app/src/main/java/ai/tomorrow/instagramclone/Share/GalleryFragment.java
+++ app/src/main/java/ai/tomorrow/instagramclone/Share/GalleryFragment.java
@@ -1,5 +1,6 @@
 package ai.tomorrow.instagramclone.Share;
 
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -17,14 +18,24 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.assist.FailReason;
+import com.nostra13.universalimageloader.core.listener.ImageLoadingListener;
+
 import java.util.ArrayList;
 
 import ai.tomorrow.instagramclone.R;
 import ai.tomorrow.instagramclone.Utils.FilePaths;
 import ai.tomorrow.instagramclone.Utils.FileSearch;
+import ai.tomorrow.instagramclone.Utils.GridImageAdapter;
 
 public class GalleryFragment extends Fragment {
     private static final String TAG = "GalleryFragment";
+
+    //constants
+    private static final int NUM_GRID_COLUMNS = 4;
+    private static final String mAppend = "file:/";
+
     
     //widgets
     private GridView gridView;
@@ -96,6 +107,7 @@ public class GalleryFragment extends Fragment {
                 Log.d(TAG, "onItemSelected: selected: " + directories.get(position));
 
                 // setup our image grid for the directory chosen
+                setupGridView(directories.get(position));
             }
 
             @Override
@@ -103,6 +115,66 @@ public class GalleryFragment extends Fragment {
 
             }
         });
+
+    }
+
+    private void setupGridView(String selectedDirectory){
+        Log.d(TAG, "setupGridView: directory chosen: " + selectedDirectory);
+        final ArrayList<String> imgURLs = FileSearch.getFilePaths(selectedDirectory);
+
+        // set the grid column width
+        int gridWidth = getResources().getDisplayMetrics().widthPixels;
+        int imageWidth = gridWidth / NUM_GRID_COLUMNS;
+        gridView.setColumnWidth(imageWidth);
+
+        // user the grid adapter to adapter the images to gridview
+        GridImageAdapter adapter = new GridImageAdapter(getActivity(), R.layout.layout_grid_imageview, mAppend, imgURLs);
+        gridView.setAdapter(adapter);
+
+        // set the first image to be displayed when the activity fragment is inflated.
+        if (imgURLs != null && imgURLs.size() > 0){
+            setImage(imgURLs.get(0), galleryImage, mAppend);
+        }
+        
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Log.d(TAG, "onItemClick: selected an image + " + imgURLs.get(position));
+                if (imgURLs != null && imgURLs.size() > 0){
+                    setImage(imgURLs.get(position), galleryImage, mAppend);
+                }
+            }
+        });
+        
+    }
+
+    private void setImage(String imgURL, ImageView image, String append){
+        Log.d(TAG, "setImage: setting image");
+
+        ImageLoader imageLoader = ImageLoader.getInstance();
+
+        imageLoader.displayImage(append + imgURL, image, new ImageLoadingListener() {
+            @Override
+            public void onLoadingStarted(String imageUri, View view) {
+                progressBar.setVisibility(View.VISIBLE);
+            }
+
+            @Override
+            public void onLoadingFailed(String imageUri, View view, FailReason failReason) {
+                progressBar.setVisibility(View.GONE);
+            }
+
+            @Override
+            public void onLoadingComplete(String imageUri, View view, Bitmap loadedImage) {
+                progressBar.setVisibility(View.GONE);
+            }
+
+            @Override
+            public void onLoadingCancelled(String imageUri, View view) {
+                progressBar.setVisibility(View.GONE);
+            }
+        });
+
 
     }
 
