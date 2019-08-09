86944566f122d9569a97915cd516069ceddbad9b
Yini <ydxb7@mail.missouri.edu>
2019-08-08 18:15:07 -0700

    79. Inserting Comments

--- app/src/main/java/ai/tomorrow/instagramclone/Utils/ViewCommentsFragment.java
+++ app/src/main/java/ai/tomorrow/instagramclone/Utils/ViewCommentsFragment.java
@@ -1,20 +1,32 @@
 package ai.tomorrow.instagramclone.Utils;
 
-import android.media.Image;
+import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.StorageReference;
+
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
+import java.util.Locale;
+import java.util.TimeZone;
 
 import ai.tomorrow.instagramclone.R;
 import ai.tomorrow.instagramclone.models.Comment;
@@ -29,13 +41,21 @@ public class ViewCommentsFragment extends Fragment {
     }
 
     //widgets
-    private ImageView mBackArrow, mCheckMard;
+    private ImageView mBackArrow, mCheckMark;
     private EditText mComment;
     private ListView mListView;
 
     //vars
     private Photo mPhoto;
     private ArrayList<Comment> mComments;
+
+    // Firebase
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+    private String userID;
+    private FirebaseDatabase mFirebaseDatabase;
+    private DatabaseReference myRef;
+    private StorageReference mStorageReference;
 
     @Nullable
     @Override
@@ -44,10 +64,11 @@ public class ViewCommentsFragment extends Fragment {
         Log.d(TAG, "onCreateView: started.");
 
         mBackArrow = view.findViewById(R.id.backArrow);
-        mCheckMard = view.findViewById(R.id.ivPostComment);
+        mCheckMark = view.findViewById(R.id.ivPostComment);
         mComment = view.findViewById(R.id.comment);
         mListView = view.findViewById(R.id.listView);
 
+        setupFirebaseAuth();
 
         try {
             mPhoto = getPhotoFromBundle();
@@ -66,11 +87,64 @@ public class ViewCommentsFragment extends Fragment {
         CommentListAdapter adapter = new CommentListAdapter(getActivity(), R.layout.layout_comment, mComments);
         mListView.setAdapter(adapter);
 
+        mCheckMark.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!mComment.getText().toString().equals("")){
+                    Log.d(TAG, "onClick: post a comment.");
+                    addNewComment(mComment.getText().toString());
+
+                    mComment.setText("");
+                    closeKeyboard();
+                } else {
+                    Toast.makeText(getActivity(), "you can't post an empty comment.", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+
+
         return view;
     }
 
+    private void closeKeyboard(){
+        View view = getActivity().getCurrentFocus();
+        if (view != null){
+            InputMethodManager imm = (InputMethodManager) getActivity().getSystemService(Context.INPUT_METHOD_SERVICE);
+            imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+        }
+    }
 
+    private void addNewComment(String newComment){
+        Log.d(TAG, "addNewComment: adding new comment: " + newComment);
+        Comment comment = new Comment();
+        comment.setComment(newComment);
+        comment.setUser_id(FirebaseAuth.getInstance().getCurrentUser().getUid());
+        comment.setDate_created(getTimeStamp());
 
+        String commentID = myRef.push().getKey();
+
+        //insert into photos node
+        myRef.child(getString(R.string.dbname_photos))
+                .child(mPhoto.getPhoto_id())
+                .child(getString(R.string.field_comments))
+                .child(commentID)
+                .setValue(comment);
+
+        //insert into user_photos node
+        myRef.child(getString(R.string.dbname_user_photos))
+                .child(mPhoto.getUser_id())
+                .child(mPhoto.getPhoto_id())
+                .child(getString(R.string.field_comments))
+                .child(commentID)
+                .setValue(comment);
+    }
+
+    private String getTimeStamp(){
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'", Locale.US);
+        sdf.setTimeZone(TimeZone.getTimeZone("America/Los_Angeles"));
+        return sdf.format(new Date());
+    }
 
     /**
      * return the photo from the incoming bundle from profileActivity interface
@@ -86,4 +160,50 @@ public class ViewCommentsFragment extends Fragment {
             return null;
         }
     }
+
+    /**
+     * -------------------------------- firebase --------------------------
+     */
+
+    /**
+     * setup the firebase auth object
+     */
+    private void setupFirebaseAuth() {
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
+        mFirebaseDatabase = FirebaseDatabase.getInstance();
+        myRef = mFirebaseDatabase.getReference();
+
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = mAuth.getCurrentUser();
+
+                if (user != null) {
+                    // User is logged in
+                    Log.d(TAG, "onAuthStateChanged: signed_in: " + user.getUid());
+                } else {
+                    // User is signed out
+                    Log.d(TAG, "onAuthStateChanged: signed_out");
+                }
+            }
+        };
+
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
+    }
+
+
 }
