6efb77ebba65a12dc6fb3a62598a6bea829c0af4
Yini <ydxb7@mail.missouri.edu>
2019-08-08 23:40:32 -0700

    82. Search Acitivty Layout

--- app/src/main/java/ai/tomorrow/instagramclone/Search/SearchActivity.java
+++ app/src/main/java/ai/tomorrow/instagramclone/Search/SearchActivity.java
@@ -5,13 +5,27 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+import android.widget.ListView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
+
+import java.util.List;
 
 import ai.tomorrow.instagramclone.R;
 import ai.tomorrow.instagramclone.Utils.BottomNavigationViewHelper;
+import ai.tomorrow.instagramclone.models.User;
 
 public class SearchActivity extends AppCompatActivity {
 
@@ -20,12 +34,69 @@ public class SearchActivity extends AppCompatActivity {
 
     private Context mContext = SearchActivity.this;
 
+    //widegets
+    private EditText mSearch;
+    private ListView mListView;
+
+    //vars
+    private List<User> mUserList;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        setContentView(R.layout.activity_search);
         Log.d(TAG, "onCreate: starting");
+        mSearch = (EditText) findViewById(R.id.search);
+        mListView = (ListView) findViewById(R.id.listView);
+        hideSoftKeyboard();
+
         setupBottomNavigationView();
+
+
+
+
+
+
+
+
+    }
+
+    private void searchForMatch(String keyword){
+        Log.d(TAG, "searchForMatch: searching for: " + keyword);
+        mUserList.clear();
+        if (!keyword.equals("")){
+            DatabaseReference reference = FirebaseDatabase.getInstance().getReference();
+
+            Query query = reference.child(getString(R.string.dbname_users))
+                    .orderByChild(getString(R.string.field_username))
+                    .equalTo(keyword);
+            query.addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                    for (DataSnapshot singleDatasnapshot: dataSnapshot.getChildren()){
+                        Log.d(TAG, "onDataChange: getting user: " + singleDatasnapshot.getValue(User.class).toString());
+                        mUserList.add(singleDatasnapshot.getValue(User.class));
+                    }
+                    // update the users list view
+
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                }
+            });
+        }
+
+
+    }
+
+    private void hideSoftKeyboard(){
+        if (getCurrentFocus() != null){
+            InputMethodManager imm = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
+            imm.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), 0);
+        }
     }
 
     /**
