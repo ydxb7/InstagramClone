07e10f268a889c9bec791d4df2aa252cbc8ed8cd
Yini <ydxb7@mail.missouri.edu>
2019-08-17 13:53:02 -0700

    change some icon

--- app/src/main/java/ai/tomorrow/instagramclone/Utils/BottomNavigationViewHelper.java
+++ app/src/main/java/ai/tomorrow/instagramclone/Utils/BottomNavigationViewHelper.java
@@ -59,7 +59,7 @@ public class BottomNavigationViewHelper {
                         callingActivity.overridePendingTransition(R.anim.fade_in, R.anim.fade_out);
                         break;
 
-                    case R.id.ic_android:
+                    case R.id.ic_account:
                         Intent intent4 = new Intent(context, ProfileActivity.class); // ACTIVITY_NUM = 4
                         context.startActivity(intent4);
                         callingActivity.overridePendingTransition(R.anim.fade_in, R.anim.fade_out);
