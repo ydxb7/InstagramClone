c8fd40c9efdb360bdf750a6f11cd00c1b7c2a2f2
Yini <ydxb7@mail.missouri.edu>
2019-08-19 18:59:30 -0700

    set likes number

--- app/src/main/java/ai/tomorrow/instagramclone/Utils/CommentListAdapter.java
+++ app/src/main/java/ai/tomorrow/instagramclone/Utils/CommentListAdapter.java
@@ -32,6 +32,7 @@ import java.util.TimeZone;
 
 import ai.tomorrow.instagramclone.R;
 import ai.tomorrow.instagramclone.models.Comment;
+import ai.tomorrow.instagramclone.models.Like;
 import ai.tomorrow.instagramclone.models.UserAccountSettings;
 import de.hdodenhof.circleimageview.CircleImageView;
 
@@ -104,40 +105,7 @@ public class CommentListAdapter extends ArrayAdapter<Comment> {
             holder.heart_red.setVisibility(View.GONE);
         } else {
             // set heart init
-            Query query2 = myRef
-                    .child(mContext.getString(R.string.dbname_photos))
-                    .child(photoID)
-                    .child(mContext.getString(R.string.field_comments))
-                    .child(getItem(position).getComment_id())
-                    .child(mContext.getString(R.string.field_likes))
-                    .orderByChild(mContext.getString(R.string.field_liked_by_user_id))
-                    .equalTo(FirebaseAuth.getInstance().getCurrentUser().getUid());
-
-            query2.addValueEventListener(new ValueEventListener() {
-                @Override
-                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                    Log.d(TAG, "onDataChange: comment's like state change.");
-                    mLikedByCurrentUser = false;
-                    for (DataSnapshot singleSnapshot: dataSnapshot.getChildren()){
-                        Log.d(TAG, "onDataChange: found comment like: " + singleSnapshot);
-                        mLikedByCurrentUser = true;
-                    }
-                    Log.d(TAG, "onDataChange: comment mLikedByCurrentUser: " + mLikedByCurrentUser);
-
-                    if (mLikedByCurrentUser){
-                        holder.heart_red.setVisibility(View.VISIBLE);
-                        holder.heart_white.setVisibility(View.GONE);
-                    } else {
-                        holder.heart_red.setVisibility(View.GONE);
-                        holder.heart_white.setVisibility(View.VISIBLE);
-                    }
-                }
-
-                @Override
-                public void onCancelled(@NonNull DatabaseError databaseError) {
-
-                }
-            });
+            setCommentLike(position, holder);
 
             // set heart onclick
             holder.heart_white.setOnClickListener(new View.OnClickListener() {
@@ -146,6 +114,7 @@ public class CommentListAdapter extends ArrayAdapter<Comment> {
                     Log.d(TAG, "onClick: liking comment.");
 
                     mFirebaseMethods.addCommentNewLike(photoID, photoOwnerID, getItem(position).getComment_id(), getItem(position).getUser_id());
+                    setCommentLike(position, holder);
                 }
             });
 
@@ -155,6 +124,7 @@ public class CommentListAdapter extends ArrayAdapter<Comment> {
                     Log.d(TAG, "onClick: liking comment.");
 
                     mFirebaseMethods.removeCommentLike(photoID, photoOwnerID, getItem(position).getComment_id());
+                    setCommentLike(position, holder);
                 }
             });
         }
@@ -193,6 +163,51 @@ public class CommentListAdapter extends ArrayAdapter<Comment> {
             }
         });
 
+    }
+
+    private void setCommentLike(int position, final ViewHolder holder) {
+        Query query = myRef
+                .child(mContext.getString(R.string.dbname_photos))
+                .child(photoID)
+                .child(mContext.getString(R.string.field_comments))
+                .child(getItem(position).getComment_id())
+                .child(mContext.getString(R.string.field_likes));
+
+        query.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                Log.d(TAG, "onDataChange: comment's like state change.");
+                mLikedByCurrentUser = false;
+                int likeCount = 0;
+                for (DataSnapshot singleSnapshot: dataSnapshot.getChildren()){
+                    Log.d(TAG, "onDataChange: found comment like: " + singleSnapshot);
+                    if (singleSnapshot.getValue(Like.class).getLiked_by_user_id().equals(FirebaseAuth.getInstance().getCurrentUser().getUid()))
+                    mLikedByCurrentUser = true;
+                    likeCount++;
+                }
+                Log.d(TAG, "onDataChange: comment mLikedByCurrentUser: " + mLikedByCurrentUser);
+
+                if (mLikedByCurrentUser){
+                    holder.heart_red.setVisibility(View.VISIBLE);
+                    holder.heart_white.setVisibility(View.GONE);
+                } else {
+                    holder.heart_red.setVisibility(View.GONE);
+                    holder.heart_white.setVisibility(View.VISIBLE);
+                }
+
+                if (likeCount != 0){
+                    holder.likes_number.setVisibility(View.VISIBLE);
+                    holder.likes_number.setText(likeCount + " likes");
+                }else {
+                    holder.likes_number.setVisibility(View.GONE);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
     }
 
 
